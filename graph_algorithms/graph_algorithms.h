eef7d46070dd13a777db28b57e750606a1e48235
joraxorosh <joraxorosh@gmail.com>
2026-03-27 19:27:08 +0300

    part2 init

---
+++ graph_algorithms/graph_algorithms.h
@@ -0,0 +1,25 @@
+#ifndef S21_GRAPH_ALGORITHMS_H
+#define S21_GRAPH_ALGORITHMS_H
+
+#include <vector>
+#include "graph.h"
+
+struct TsmResult {
+    std::vector<int> vertices;
+    double distance;
+};
+
+class GraphAlgorithms {
+public:
+    static std::vector<int> DepthFirstSearch(const Graph& graph, int start_vertex);
+    static std::vector<int> BreadthFirstSearch(const Graph& graph, int start_vertex);
+    static int GetShortestPathBetweenVertices(const Graph& graph, int vertex1, int vertex2);
+    static std::vector<std::vector<int>> GetShortestPathsBetweenAllVertices(const Graph& graph);
+    static std::vector<std::vector<int>> GetLeastSpanningTree(const Graph& graph);
+    static TsmResult SolveTravelingSalesmanProblem(const Graph& graph);
+    // дополнительные алгоритмы для Part 6
+    static TsmResult SolveTSP_BruteForce(const Graph& graph);
+    static TsmResult SolveTSP_HeldKarp(const Graph& graph);
+};
+
+#endif // S21_GRAPH_ALGORITHMS_H
